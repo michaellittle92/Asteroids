3243aad918505af579d586be2322c15f8d1b6cc0
Michael Little <michaellittle92@gmail.com>
2025-05-24 12:37:49 +1000

    Rendering player

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from constants import * 
 import pygame
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    clock.tick()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
-
+    player = Player(SCREEN_HEIGHT/2, SCREEN_WIDTH/2)
     while True: 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick()/1000
 
 if __name__ == "__main__":
     main()
