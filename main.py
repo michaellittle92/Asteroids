3b386cb576567af3dbc3783756ac3894e328c18c
Michael Little <michaellittle92@gmail.com>
2025-05-24 14:01:01 +1000

    spawning asteroids

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from constants import * 
 import pygame
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -10,14 +12,18 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     updateable =  pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable,)
 
     player = Player(SCREEN_HEIGHT/2, SCREEN_WIDTH/2)
+    asteroid_field = AsteroidField()
     
     while True: 
         for event in pygame.event.get():
