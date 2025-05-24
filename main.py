3308c2e96ac14679946e4ce9866094a5a787dc0b
Michael Little <michaellittle92@gmail.com>
2025-05-24 11:56:50 +1000

    Configured constants set up pygame and rendering window

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+from constants import * 
 import pygame
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
+
+    while True: 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
